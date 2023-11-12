9cbb3465e6ff14388587b42a832b4299115f742f
Gabriel Odess-Gillett <gabeodess@gmail.com>
2023-11-12 14:48:09 -0600

    feat: navbar padding

--- app/javascript/components/Layout.jsx
+++ app/javascript/components/Layout.jsx
@@ -3,10 +3,10 @@ import { Link, Outlet } from "react-router-dom";
 
 export default () => {
     return <div>
-        <div className="container">
-            <Outlet />
-        </div>
-        <nav className="navbar fixed-bottom bg-body-tertiary">
+      <div className="container">
+          <Outlet />
+      </div>
+      <nav className="navbar fixed-bottom bg-body-tertiary pb-4">
         <div className="container-fluid justify-content-around">
           <Link className='fs-1 btn btn-outline-secondary border-0' to="/"><i className="bi bi-calendar"></i></Link>
           <Link className='fs-1 btn btn-outline-secondary border-0' to="/day-ones/new"><i className="bi bi-plus-square"></i></Link>
