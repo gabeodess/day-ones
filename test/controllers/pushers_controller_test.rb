12b9bd450a3470a0111dc0fd798fa68a9a205bb5
Gabriel Odess-Gillett <gabeodess@gmail.com>
2023-12-28 16:56:16 -0600

    fix: pusher tests

--- test/controllers/pushers_controller_test.rb
+++ test/controllers/pushers_controller_test.rb
@@ -2,7 +2,10 @@ require "test_helper"
 
 class PushersControllerTest < ActionDispatch::IntegrationTest
   test "should get create" do
-    get pushers_create_url
-    assert_response :success
+    sign_in(users(:one))
+    assert_difference "Pusher.count" do
+      post pushers_url, params: {pusher: {endpoint: :foobar, auth: :foobar, p256dh: :foobar}}
+      assert_response :success
+    end
   end
 end
